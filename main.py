df8d82e0bb4ad09e3d821b6e1f37b3ca62358ffc
VindiaFres <45542825+VindiaFres@users.noreply.github.com>
2023-10-31 17:19:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,34 @@ import numpy as np
 import base64
 from PIL import ImageOps, Image
 
-def set_background(image_file):
-    """
-        image_file (str): The path to the image file to be used as the background.
 
-    """
+def application(image, model, labels):
+    #image: Photo de l'oeil , model: Le modèle,
+    
+    #on mets l'image à la bonne taille
+    image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
+
+    # transforme l'image en tableau numpy
+    image_array = np.asarray(image)
+
+    #adapte pour le moele
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # dataset
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data[0] = normalized_image_array
+
+    #  ml
+    prediction = model.predict(data)
+    index = 0 if prediction[0][0] > 0.95 else 1
+    label = labels[index]
+    confidence_score = prediction[0][index]
+
+    return label, confidence_score
+
+
+def fond_ecran(image_file): #image_file: le chemin d'acces de l'image.
+       
     with open(image_file, "rb") as f:
         img_data = f.read()
     b64_encoded = base64.b64encode(img_data).decode()
@@ -22,38 +45,8 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
-
-def classify(image, model, class_names):
-    """
-        image (PIL.Image.Image): Photo de l'oeil 
-        model (tensorflow.keras.Model): Le modèle
-        class_names (list)
-    """
-    # convert image to (224, 224)
-    image = ImageOps.fit(image, (224, 224), Image.Resampling.LANCZOS)
-
-    # convert image to numpy array
-    image_array = np.asarray(image)
-
-    # normalize image
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-
-    # set model input
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    data[0] = normalized_image_array
-
-    # make prediction
-    prediction = model.predict(data)
-    # index = np.argmax(prediction)
-    index = 0 if prediction[0][0] > 0.95 else 1
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-
-    return class_name, confidence_score
-
-
 # Main 
-set_background('./fondecran/fe.png') #fond d'écran
+fond_ecran('./fondecran/fe.png') #fond d'écran
 st.title('Eagle Eye 🦅')
 st.write("")
 st.title('Disease detection')
@@ -67,7 +60,7 @@ model = load_model('./model/detectmaladie.h5')
 
 # charge le nom des deux classes
 with open('./model/labels.txt', 'r') as f:
-    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+    labels = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
 #affiche l'image chargé et si elle est malade ou pas 
@@ -75,11 +68,11 @@ if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
-    class_name, conf_score = classify(image, model, class_names)
+    label, conf_score = application(image, model, labels)
 
     # ecrit les résultats
-    st.write("## {}".format(class_name))
+    st.write("## {}".format(label))
     st.write("### score: {}%".format(int(conf_score * 1000) / 10))
-    if class_name== "Healthy":
+    if label== "Healthy":
         emoji = "😄"
         st.title(f"{emoji}")
