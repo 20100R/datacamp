b4ac6ad6c335b2f7c1f17ff40e2043ce1bb312fd
VindiaFres <45542825+VindiaFres@users.noreply.github.com>
2023-10-31 14:29:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
-
 from PIL import Image
 import numpy as np
-
 from util import classify, set_background
 
 
@@ -29,12 +27,11 @@ if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
-    # classify image
     class_name, conf_score = classify(image, model, class_names)
 
-    # write classification
+    # ecrit les résultats
     st.write("## {}".format(class_name))
     st.write("### score: {}%".format(int(conf_score * 1000) / 10))
     if class_name== "Normale":
         emoji = "😄"
-        st.write(f"{emoji}")
+        st.title(f"{emoji}")
