15f7bf0a962e1299e46c93d76f0c17b356e5563a
VindiaFres <45542825+VindiaFres@users.noreply.github.com>
2023-10-31 14:18:30 +0100

    Update util.py

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import base64
-
 import streamlit as st
 from PIL import ImageOps, Image
 import numpy as np
@@ -7,13 +6,8 @@ import numpy as np
 
 def set_background(image_file):
     """
-    This function sets the background of a Streamlit app to an image specified by the given image file.
-
-    Parameters:
         image_file (str): The path to the image file to be used as the background.
 
-    Returns:
-        None
     """
     with open(image_file, "rb") as f:
         img_data = f.read()
@@ -31,10 +25,7 @@ def set_background(image_file):
 
 def classify(image, model, class_names):
     """
-    This function takes an image, a model, and a list of class names and returns the predicted class and confidence
-    score of the image.
 
-    Parameters:
         image (PIL.Image.Image): An image to be classified.
         model (tensorflow.keras.Model): A trained machine learning model for image classification.
         class_names (list): A list of class names corresponding to the classes that the model can predict.
